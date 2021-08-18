cb807279c8a5316d5af477da2c860add8c9bff8d
Tomasz Janczuk <tomasz@janczuk.org>
2021-08-17 18:22:18 -0700

    add cors

--- routes/cat.js
+++ routes/cat.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../lib/db');
 const createError = require('http-errors')
 const Joi = require('joi');
+const Cors = require('cors');
 
 const catSchema = Joi.object().keys({
     name: Joi.string(),
@@ -11,7 +12,17 @@ const catSchema = Joi.object().keys({
     tags: Joi.object().keys().pattern(/./, Joi.string()),
 });
 
-router.get('/', function(req, res) {
+const corsOptions = {
+    origins: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    exposedHeaders: 'content-length',
+    credentials: true,
+};
+
+router.options('/', Cors(corsOptions));
+router.options('/:id', Cors(corsOptions));
+
+router.get('/', Cors(corsOptions), function(req, res) {
     const count = +(req.query.count || 5);
     const next = +(req.query.next || 0);
     delete req.query.count;
@@ -19,16 +30,16 @@ router.get('/', function(req, res) {
     return res.json(db.search(req.query, count, next));
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', Cors(corsOptions), function(req, res, next) {
     const cat = db.tryGet(req.params.id);
     return cat ? res.json(cat) : next(createError(404, 'Cat not found'));
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', Cors(corsOptions), function(req, res, next) {
     return db.delete(req.params.id) ? res.status(204).send() : next(createError(404, 'Cat not found'));
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', Cors(corsOptions), function(req, res, next) {
     const result = catSchema.validate(req.body);
     if (result.error) {
         return next(createError(400, result.error.message));
